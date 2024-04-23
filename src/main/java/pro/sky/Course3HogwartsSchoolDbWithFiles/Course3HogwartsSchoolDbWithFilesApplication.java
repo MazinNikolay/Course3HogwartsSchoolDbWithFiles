f2f72c415ed09913a2e043dfb8ea69db1bfa7698
kolia <koliamba_85@mail.ru>
2024-04-23 22:53:23 +0800

    Исходный коммит

---
+++ src/main/java/pro/sky/Course3HogwartsSchoolDbWithFiles/Course3HogwartsSchoolDbWithFilesApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Course3HogwartsSchoolDbWithFiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course3HogwartsSchoolDbWithFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course3HogwartsSchoolDbWithFilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/Course3HogwartsSchoolDbWithFiles/Course3HogwartsSchoolDbWithFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Course3HogwartsSchoolDbWithFiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course3HogwartsSchoolDbWithFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
