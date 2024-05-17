ab985bde4b1cdd9b5d551acc18e5b6cec05f5df6
kolia <koliamba_85@mail.ru>
2024-05-17 22:08:28 +0800

    Домашка выполнена

---
+++ script423.sql
@@ -0,0 +1,2 @@
+select student.name, student.age, faculty.name from student inner join faculty on student.faculty_id = faculty.id
+select student.name, student.age from student right join avatar on avatar.student_id = student.id
